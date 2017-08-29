c02fd014b9ae005576eb1d1f0c8e63f2f99a45d3
Txuritan <txurtian@yahoo.com>
2017-08-29 13:19:00 -0400

    GDX project

---
+++ android/src/com/github/txuritan/loflp/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.github.txuritan.loflp;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.github.txuritan.loflp.LotLPApp;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new LotLPApp(), config);
+	}
+}
---
+++ desktop/src/com/github/txuritan/loflp/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.github.txuritan.loflp.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.github.txuritan.loflp.LotLPApp;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LotLPApp(), config);
+	}
+}
